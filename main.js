423f5c176196f5511b62acbef839dadada940e46
Hollas <nicholasolajide@gmail.com>
2023-08-31 12:15:23 +0100

    javascript

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-cta = document.getElementById("cta");
+let frm = document.getElementById("frm"),
 vid = document.getElementById("vid");
 
-function clicker() {
-  if (cta.style.display =='block') {
-    cta.style.display = 'none';
+function validateForm() {
+  let x, i, valid = true;
+  x = document.getElementsByTagName("input");
+  for (let i = 0; i < x.length; i++) {
+    // if field is empty
+    if (x[i].value == "") {
+      alert("Input field can't be empty");
+      valid = false;
+    }
+  }
+
+  if (valid) {
+    frm.style.display = 'none';
     vid.style.display = 'block';
   }
+  return valid
+}
+
+function clicker() {
+ validateForm();
 }
