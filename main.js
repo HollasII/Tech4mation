72209364787f327f4866a5d54ec9f53ea59bd514
Hollas <nicholasolajide@gmail.com>
2023-08-31 11:40:03 +0100

    finished work

---
+++ main.js
@@ -0,0 +1,9 @@
+cta = document.getElementById("cta");
+vid = document.getElementById("vid");
+
+function clicker() {
+  if (cta.style.display =='block') {
+    cta.style.display = 'none';
+    vid.style.display = 'block';
+  }
+}
